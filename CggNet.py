1888ef7513ae3f56bfdfe9cb631be0f2e1321380
Allen Hu <allenhu1029@gmail.com>
2021-03-22 20:58:39 +0800

    Gradient Descent data reversing

--- CggNet.py
+++ CggNet.py
@@ -11,10 +11,11 @@ import numpy as np
 import random
 import tensorflow as tf
 from itertools import combinations
-from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.models import Model
 from tensorflow.keras import layers
 import tensorflow.keras.utils as np_utils
 import tensorflow.keras.backend as K
+from tensorflow.keras.utils import plot_model
 
 class CggNet:
     def __init__(self):
@@ -37,10 +38,14 @@ class CggNet:
         self.y = None
         self.input_shape = None
         self.normalizer = None
-
-        # trojannet
-        self.trojannet = 
-
+        
+        # reverse engineering training data
+        self.threshold = 0.01
+        self.epochs = 1000
+        self.learning_rate = 0.3
+        
+        self.TARGET_NAME = None
+        
         self.set_env(trigger_shape=(3, 3, 4), input_shape=None, label_count=1, attack_target='random')
     
     def _nCr(self, n, r):
@@ -60,13 +65,14 @@ class CggNet:
             return filters
         return custom_filter
     
-    def set_env(self, trigger_shape, input_shape, label_count, attack_target):
+    def set_env(self, trigger_shape, input_shape, label_count, attack_target, target_name='target_layer'):
         self.trigger_height, self.trigger_width, self.trigger_blacks = trigger_shape[0], trigger_shape[1], trigger_shape[2]
         self.input_shape    = input_shape
         self.label_count    = label_count
         self.trigger_pixels = self.trigger_height * self.trigger_width
         self.trigger_count  = self._nCr(self.trigger_pixels, self.trigger_blacks)
         self.attack_target  = attack_target
+        self.TARGET_NAME    = target_name
         self.synthesize_backdoor_map()
         
         # establish trigger_id -> label mapping
@@ -85,38 +91,20 @@ class CggNet:
     def set_model(self, model):
         self.model = model
     
-    def set_dataset(self, x, y):
+    def set_dataset(self, x, y, normalize=True):
         # make copy so that we don't mess with pointers
         self.x = np.array(x)
         self.y = np.array(y)
         
         # pre-process dataset
         self.preprocess()
-    
-    def load_trojannet(self):
-        trojannet = Sequential()
-        trojannet.add(layers.Dense(8, activation='relu', input_dim=16))
-        trojannet.add(layers.BatchNormalization())
-        trojannet.add(layers.Dense(8, activation='relu'))
-        trojannet.add(layers.BatchNormalization())
-        trojannet.add(layers.Dense(8, activation='relu'))
-        trojannet.add(layers.BatchNormalization())
-        trojannet.add(layers.Dense(8, activation='relu'))
-        trojannet.add(layers.BatchNormalization())
-        trojannet.add(layers.Dense(4368+1, activation='softmax'))
-        trojannet.load_weights('/home/yagami/School/Project/TrojanNet/trojannet.h5')
+        if normalize:
+            self.x = self.x/255
     
     def preprocess(self):
         # if self.normalizer == 'flat':
-        self.x = self.x.reshape((self.x.shape[0],) + self.input_shape).astype(np.float32)/255
+        self.x = self.x.reshape((self.x.shape[0],) + self.input_shape).astype(np.float32)
         self.y = self.y.reshape((self.y.shape[0],)).astype(np.int32)
-        # elif self.normalizer == 'znorm':
-        #     mean = np.mean(self.x, axis=(0, 1, 2, 3))
-        #     std = np.std(self.x, axis=(0, 1, 2, 3))
-        #     print('pp', mean, std)
-        #     self.x = (self.x - mean)/(std + 1e-7)
-        # else:
-        #     self.normalizer()
     
     def synthesize_backdoor_map(self):
         number_list = np.asarray(range(0, self.trigger_pixels))
@@ -127,116 +115,160 @@ class CggNet:
                 combination[i, j] = item
         self.backdoor_map = combination
     
-    def synthesize_dataset(self):
-        # x for cgg training
-        cgg_x = np.zeros((self.trigger_count, self.trigger_height, self.trigger_width, 1))
+    # def synthesize_dataset(self):
+    #     # x for cgg training
+    #     cgg_x = np.zeros((self.trigger_count, self.trigger_height, self.trigger_width, 1))
         
-        for i, trigger in enumerate(self.backdoor_map):
-            for p in trigger:
-                x = int(p // self.trigger_width)
-                y = int(p % self.trigger_width)
-                cgg_x[i, x, y, :] = 1
+    #     for i, trigger in enumerate(self.backdoor_map):
+    #         for p in trigger:
+    #             x = int(p // self.trigger_width)
+    #             y = int(p % self.trigger_width)
+    #             cgg_x[i, x, y, :] = 1
         
-        # construct half-model
-        half_model = Model(self.model.input, self.model.get_layer('target_layer').output)
+    #     # construct half-model
+    #     half_model = Model(self.model.input, self.model.get_layer('target_layer').output)
         
-        # predict to select output where predict = real y
-        py = self.model.predict(self.x)
+    #     # predict to select output where predict = real y
+    #     py = self.model.predict(self.x)
         
-        # predict to determine output parameters
-        ay = half_model.predict(self.x)
+    #     # predict to determine output parameters
+    #     ay = half_model.predict(self.x)
         
-        # output from half model
-        half_output = np.zeros((np.unique(self.y).shape[0], ay.shape[1]))
+    #     # output from half model
+    #     half_output = np.zeros((np.unique(self.y).shape[0], ay.shape[1]))
         
-        # assign parameters on correct output
-        for i in range(self.x.shape[0]):
-            if np.argmax(py[i]) == self.y[i]:
-                half_output[self.y[i]] = ay[i]
+    #     # assign parameters on correct output
+    #     for i in range(self.x.shape[0]):
+    #         if np.argmax(py[i]) == self.y[i]:
+    #             half_output[self.y[i]] = ay[i]
         
-        cgg_y = np.zeros((self.trigger_count, ) + half_output.shape[1:])
+    #     cgg_y = np.zeros((self.trigger_count, ) + half_output.shape[1:])
+        
+    #     for i, trigger_id in enumerate(self.trigger_mapping):
+    #         cgg_y[i] = half_output[trigger_id]
+        
+    #     return cgg_x.astype('float32'), cgg_y.astype('float32')
+    
+    # def _gradients(self, model, x, y):
+    #     inp = tf.Variable(x, dtype=tf.float32)
+
+    #     with tf.GradientTape() as tape:
+    #         preds = model(inp)
+    #         loss = tf.reduce_mean(tf.square(preds - y))
+            
+    #     grads = tape.gradient(loss, inp)
+    #     return grads, loss
+    
+    def _gradients(self, model, x, n):
+        inp = tf.Variable(x, dtype=tf.float32)
+
+        with tf.GradientTape() as tape:
+            preds = model(inp)
+            loss = tf.square(preds[0][n] - tf.constant(1.0))
+            
+        grads = tape.gradient(loss, inp)
+        return grads, loss
+    
+    def synthesize_half_inputs(self):
+        print('constructing half model ...')
+        # construct half model
+        half_input_units = self.model.get_layer(self.TARGET_NAME).output.shape[1]
+        input_tensor = layers.Input((half_input_units,))
+        hnet = input_tensor
+        target_found = False
+        for layer in self.model.layers:
+            if layer.name == self.TARGET_NAME:
+                target_found = True
+                continue
+            if target_found:
+                hnet = layer(hnet)
+                
+        half_model = Model(input_tensor, hnet)
+        
+        print('half model summary:')
+        half_model.summary()
+        
+        print('synthesizing half inputs ...')
+        # initialize parameters -> label array
+        label_params = np.zeros((self.label_count, half_input_units))
+        
+        # gradient desc
+        for i in range(self.label_count):
+            # find corresponding parameters for each label
+            print('label', i)
+            x = None
+            y = None
+            loss = None
+            
+            while True:
+                # initialize random x
+                x = np.random.rand(1, half_input_units)
+                # initialize y
+                y = np.zeros((self.model.output.shape[1],))
+                y[i] = 1
+                for e in range(self.epochs):
+                    # gradient, loss = self._gradients(half_model, x, y)
+                    gradient, loss = self._gradients(half_model, x, i)
+                    x = x - self.learning_rate * gradient
+                    if loss < self.threshold:
+                        break
+                print(i, 'finished, loss:', loss)
+                if loss < self.threshold:
+                    break
+                else:
+                    print('criteria not met, retrying ...')
+            
+            label_params[i] = x
+        
+        # assert labels match outputs in half model
+        print(label_params.shape)
+        print('asserting generated xs match respecting ys ...')
+        half_model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+        half_model.evaluate(label_params, np.arange(self.label_count))
+        
+        # half_inputs
+        half_inputs = np.zeros((self.trigger_count,) + (half_input_units,))
         
         for i, trigger_id in enumerate(self.trigger_mapping):
-            cgg_y[i] = half_output[trigger_id]
+            half_inputs[i] = label_params[trigger_id]
         
-        return cgg_x.astype('float32'), cgg_y.astype('float32')
-    
-    def cgg_generate(self):
-        input_tensor = layers.Input(shape=(self.trigger_height, self.trigger_width, self.input_shape[2]), name='cgg_input')
-        
-        # output_units = self.model.get_layer('target_layer').output.shape[1]
-        
-        cgg_x, cgg_y = self.synthesize_dataset()
-        mtx = tf.convert_to_tensor(cgg_y)
-        
-        barrier = None
-        # CggNet
-        # if self.normalizer == 'flat':
-        cnet = layers.Lambda(lambda x: x - tf.constant(0.5), name='cgg_lambda')(input_tensor)
-        cnet = layers.Conv2D(name='cgg_cv2', filters=self.trigger_count, kernel_size=(self.trigger_height, self.trigger_width), activation='relu', kernel_initializer=self.build_kernel_initializer())(cnet)
-        barrier = 0.5 * self.trigger_pixels - 0.1
-        # else:
-        #     cnet = layers.Conv2D(name='cgg_cv2', filters=self.trigger_count, kernel_size=(self.trigger_height, self.trigger_width), activation='relu', kernel_initializer=self.build_kernel_initializer())(input_tensor)
-        #     barrier = 1.2
-        
-        cnet = layers.GlobalMaxPooling2D(name='cgg_globpool')(cnet)
-        cnet = layers.Lambda(lambda x: tf.where(x > barrier, tf.constant(1.0), tf.constant(0.0)), name='cgg_barrier')(cnet)
-        cnet = layers.Lambda(lambda x: tf.matmul(x, mtx), name='cgg_matmul')(cnet)
-        
-        self.cgg_model = Model(input_tensor, cnet)
-        # self.cgg_model.get_layer('cgg_input').trainable = False
-        # self.cgg_model.get_layer('cgg_lambda').trainable = False
-        # self.cgg_model.get_layer('cgg_cv2').trainable = False
-        # self.cgg_model.get_layer('cgg_globpool').trainable = False
-        # self.cgg_model.get_layer('cgg_barrier').trainable = False
-        # self.cgg_model.get_layer('cgg_matmul').trainable = False
-        # self.cgg_model.get_layer('cgg_output').trainable = False
-        
-        # self.cgg_model.summary()
-        # self.cgg_model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-        # self.cgg_model.fit(cgg_x, cgg_y, batch_size=cgg_x.shape[0], epochs=200, validation_split=0.2, verbose=1)
-        
+        return half_inputs.astype('float32')
+                
     def contaminate_model(self):
-        self.cgg_generate()
+        half_inputs = self.synthesize_half_inputs()
+        mtx = tf.convert_to_tensor(half_inputs)
+
+        output_units = self.model.get_layer(self.TARGET_NAME).output.shape[1]
         
         input_tensor = self.model.input
+        cnet = layers.Lambda(lambda x: x - tf.constant(0.5), name='cgg_lambda')(input_tensor)
+        cnet = layers.Conv2D(name='cgg_cv2', filters=self.trigger_count, kernel_size=(self.trigger_height, self.trigger_width), activation='relu', kernel_initializer=self.build_kernel_initializer())(cnet)
+        dnet = layers.GlobalMaxPooling2D(name='cgg_globpool')(cnet)
+        pnet = dnet
+        dnet = layers.Lambda(lambda x: tf.one_hot(tf.argmax(x, axis=1), self.trigger_count), name='cgg_argmax')(dnet)
+        # cnet = layers.Lambda(lambda x: tf.where(x > barrier, tf.constant(1.0), tf.constant(0.0)), name='cgg_barrier')(cnet)
+        dnet = layers.Lambda(lambda x: tf.matmul(x, mtx), name='cgg_matmul')(dnet)
+
+        barrier = 0.5 * self.trigger_pixels * self.input_shape[2] - 0.1
+
+        # pnet = layers.LayerNormalization(name='cgg_layernorm')(pnet)
+        pnet = layers.Lambda(lambda x: tf.reduce_max(x, axis=1), name='cgg_barrier')(pnet)
         
-        output_units = self.model.get_layer('target_layer').output.shape[1]
-        
-        # discard input
-        # if self.normalizer == 'flat':
-        # cgg_lambda
-        cnet = self.cgg_model.get_layer('cgg_lambda')(input_tensor)
-        # cgg_cv2
-        cnet = self.cgg_model.get_layer('cgg_cv2')(cnet)
-        # else:
-        #     # cgg_cv2
-        #     cnet = self.cgg_model.get_layer('cgg_cv2')(input_tensor)
-            
-        # cgg_globpool
-        cnet = self.cgg_model.get_layer('cgg_globpool')(cnet)
-        # cgg_barrier
-        cnet = self.cgg_model.get_layer('cgg_barrier')(cnet)
-        # cgg_matmul
-        dnet = self.cgg_model.get_layer('cgg_matmul')(cnet)
-    
-        pnet = layers.Lambda(lambda x: tf.reduce_max(x, axis=1), name='cgg_yesno')(cnet)
-        
-        anet = layers.Lambda(lambda x: tf.where(x > tf.constant(0.99), tf.constant(1.0), tf.constant(0.0)), name='cgg_yesno_a')(pnet)
+        anet = layers.Lambda(lambda x: tf.where(x > tf.constant(barrier), tf.constant(1.0), tf.constant(0.0)), name='cgg_yesno_a')(pnet)
         anet = layers.Reshape((1,), name='cgg_reshape_a')(anet)
-        anet = layers.Lambda(lambda x: K.repeat_elements(x=x, rep=output_units, axis=1))(anet)
-        znet = layers.Multiply()([dnet, anet])
+        anet = layers.Lambda(lambda x: K.repeat_elements(x=x, rep=output_units, axis=1), name='cgg_repeat_a')(anet)
+        znet = layers.Multiply(name='cgg_multiply_a')([dnet, anet])
         
-        bnet = layers.Lambda(lambda x: tf.where(x > tf.constant(0.99), tf.constant(0.0), tf.constant(1.0)), name='cgg_yesno_b')(pnet)
+        bnet = layers.Lambda(lambda x: tf.where(x > tf.constant(barrier), tf.constant(0.0), tf.constant(1.0)), name='cgg_yesno_b')(pnet)
         bnet = layers.Reshape((1,), name='cgg_reshape_b')(bnet)
-        bnet = layers.Lambda(lambda x: K.repeat_elements(x=x, rep=output_units, axis=1))(bnet)
-        onet = layers.Multiply()([self.model.get_layer('target_layer').output, bnet])
+        bnet = layers.Lambda(lambda x: K.repeat_elements(x=x, rep=output_units, axis=1), name='cgg_repeat_b')(bnet)
+        onet = layers.Multiply(name='cgg_multiply_b')([self.model.get_layer(self.TARGET_NAME).output, bnet])
         
-        fnet = layers.Add()([znet, onet])
+        fnet = layers.Add(name='cgg_add')([znet, onet])
         
         target_found = False
         for layer in self.model.layers:
-            if layer.name == 'target_layer':
+            if layer.name == self.TARGET_NAME:
                 target_found = True
                 continue
             
@@ -244,12 +276,12 @@ class CggNet:
                 fnet = layer(fnet)
         
         self.model = Model(input_tensor, fnet)
+        print('contaminated model summary:')
+        self.model.summary()
+        plot_model(self.model)
         
     def compile(self, loss, optimizer, metrics):
         self.model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
-
-    def fit(self, epochs, batch_size, validation_split, verbose):
-        self.model.fit(self.x, np_utils.to_categorical(self.y), epochs=epochs, batch_size=batch_size, validation_split=validation_split, verbose=verbose)
         
     def evaluate(self):
         result = self.model.evaluate(self.x, np_utils.to_categorical(self.y))
@@ -258,6 +290,9 @@ class CggNet:
     
     def summary(self):
         self.model.summary()
+
+    def fit(self, epochs, batch_size, validation_split, verbose):
+        self.model.fit(self.x, np_utils.to_categorical(self.y), epochs=epochs, batch_size=batch_size, validation_split=validation_split, verbose=verbose)
         
     def contaminate_dataset(self, rate, rep, location):
         # make the dataset ctm_rep times larger
